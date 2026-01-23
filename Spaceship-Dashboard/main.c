4f9c02f63dac3e6ce4dc5ac1f3fa8e5180426bb9
Roman Benz <kontakt@romanbenz.com>
2026-01-23 13:24:18 +0100

    Added User-Input

--- Spaceship-Dashboard/main.c
+++ Spaceship-Dashboard/main.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char Schiffsklasse[] = "Peter II";
-int AnzahlBesatzung = 234;
-int Missionstage = 3726;
+char Schiffsklasse[25];
+int AnzahlBesatzung;
+int Missionstage;
 long long int TripToDate = 476346739467;
 float PartLightspeed = 34.6;
 float Reichweite = 653456364.546;
 
 int main(void)
 {
+    //Starting User Input
+    printf("Schiffsklase eingeben: ");
+    scanf("%25c\n", &Schiffsklasse);
+
+    printf("Anazahl der Besatzung: ");
+    scanf("%25d\n", AnzahlBesatzung);
+
+    printf("Missionstage: ");
+    scanf("%25d\n", Missionstage);
+
+    printf("\n");
+    printf("\n");
+    printf("\n");
+
     printf("Raumschiff-Dashboard\n");
     printf("====================\n\n");
     printf("%-35s %s\n", "Schiffsklasse", Schiffsklasse);
